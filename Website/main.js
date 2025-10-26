7a9deb73c37941d27f493dfe7647eded03b02d6b
dxyk26 <demi.kuan@icloud.com>
2025-10-26 10:23:44 +0100

    search balk werkend gemaakt

---
+++ Website/main.js
@@ -0,0 +1,38 @@
+"use strict"; 
+
+function gaNaarGame () { 
+    const invoer = document.getElementById("search").value.toLowerCase().trim(); 
+    const games = {
+        "snake": "snake.html",
+        "tetris": "tetris.html",
+        "boter kaas en eieren": "boterkaaseieren.html",
+        "flappybird": "flappybird.html",
+        "steen papier schaar": "steenpapierschaar.html",
+        "doolhof": "doolhof.html"
+    };
+    
+    if (games[invoer]) {
+        window.location.href = games[invoer];
+    } else {
+        alert("Game bestaat niet");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const zoekveld = document.getElementById("search");
+    const knop = document.querySelector(".search-balk button");
+
+   if (zoekveld) { 
+    zoekveld.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            gaNaarGame();
+        }
+    });
+}
+
+    if (knop) {
+        knop.addEventListener("click", function() {
+            gaNaarGame();
+        });
+    }
+});
